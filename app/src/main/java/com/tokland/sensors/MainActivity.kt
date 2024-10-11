fd93b34a5a175992b00b57f7ad7220114e05af76
Arnau Sanchez <pyarnau@gmail.com>
2024-10-11 13:49:06 +0200

    more

--- app/src/main/java/com/tokland/sensors/MainActivity.kt
+++ app/src/main/java/com/tokland/sensors/MainActivity.kt
@@ -1,47 +1,14 @@
 package com.tokland.sensors
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.tokland.sensors.ui.theme.SensorsTheme
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            SensorsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SensorsTheme {
-        Greeting("Android")
+        println("[println] Hello World from Kotlin!")
+        Log.d("HelloWorldCmdApp", "[Log.d] Hello World from Kotlin!")
+        finish()
     }
 }
