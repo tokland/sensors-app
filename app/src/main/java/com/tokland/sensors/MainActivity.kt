0154e5b8c0fcc4fba620855d36e77c977f3be571
Arnau Sanchez <pyarnau@gmail.com>
2024-10-11 16:55:49 +0200

    more

--- app/src/main/java/com/tokland/sensors/MainActivity.kt
+++ app/src/main/java/com/tokland/sensors/MainActivity.kt
@@ -3,12 +3,19 @@ package com.tokland.sensors
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         println("[println] Hello World from Kotlin!")
-        Log.d("HelloWorldCmdApp", "[Log.d] Hello World from Kotlin!")
+
+        val fileOutputStream: FileOutputStream = openFileOutput("out-tokland.txt", MODE_PRIVATE)
+        fileOutputStream.write(("line1" + "\n").toByteArray())
+        fileOutputStream.write(("line2" + "\n").toByteArray())
+        fileOutputStream.close()
+
+        Log.d("MainActivity", "[Log.d] Hello World from Kotlin!")
         finish()
     }
 }
